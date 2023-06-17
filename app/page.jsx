2ebefdf7d95fe2da05800246e9a04b232f396e13
Matthijs Boet <m.boet@chello.nl>
2023-06-17 02:03:16 +0200

    added env to gitignore, created components

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import Feed from '@components/Feed'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Promptopia is a modern world open-source AI prompting tool to discover, create and share creative prompts.
+      </p>
+      <Feed />
+    </section>
   )
 }
 
